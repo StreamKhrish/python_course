7f00195bc6afee37e5808475791f7b6910604999
streamkhrish <khrishvarekar8@gmail.com>
2025-11-11 18:54:57 +0200

    new

---
+++ lesson_3/activity1.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title("wellcome to turtle window ")
+bord=turtle.Turtle()
+for i in range(4):
+    bord.forward(100)
+    bord.left(90)
+    i=i+1
